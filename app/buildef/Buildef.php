70b880937d79a76d6d44a8c5a4a86d5a37e0ffd3
Emilio Ormeño <eormneno@gmail.com>
2023-08-29 19:51:08 -0300

    start processing xml templates

--- app/buildef/Buildef.php
+++ app/buildef/Buildef.php
@@ -10,13 +10,39 @@ class Buildef
 	public static function start()
 	{
 		$consoleOutput = app('console-output');
-		$path = dirname(__FILE__) . '/renders/html';
+		$path = self::getHtmlTemplatesDir();
 
-		$htmlFiles = self::getFiles($path, 'html');
+		$templatesFiles = self::getFiles($path, 'html');
 
-		foreach ($htmlFiles as $file) {
-			$consoleOutput->writeln($file->getBasename());
+		foreach ($templatesFiles as $templateFile) {
+			$template = self::parseXmlFile($templateFile->getPathname());
+			$consoleOutput->writeln('Template: ' . $template);
 		}
+	}
+
+	private static function parseXmlFile($file)
+	{
+		$xml = simplexml_load_file($file);
+		if (!$xml) {
+			throw new \Exception('Error parsing XML template file: ' . $file);
+		}
+		$template_name = $xml['name'];
+		$template_params = self::parseTemplateParams($xml);
+		return $template_name;
+	}
+
+	private static function parseTemplateParams($xml)
+	{
+		$params = [];
+		foreach ($xml->param as $param) {
+			$params[] = [
+				'name' => $param['name'],
+				'type' => $param['type'],
+				'default' => $param['default'],
+				'readonly' => $param['readonly'] === 'true',
+			];
+		}
+		return $params;
 	}
 
 	private static function getFiles($path, $extension)
@@ -26,5 +52,10 @@ class Buildef
 			return $file->getExtension() === $extension;
 		});
 		return $filteredFiles;
+	}
+
+	private static function getHtmlTemplatesDir()
+	{
+		return dirname(__FILE__) . '/renders/html';
 	}
 }
