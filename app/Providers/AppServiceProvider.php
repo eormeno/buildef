7ceabe85abf3056fd1338d26bd31fdd57d00225e
DAMO\Emilio <eormeno@gmail.com>
2023-08-29 12:29:40 -0300

    small changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,29 +2,30 @@
 
 namespace App\Providers;
 
-use App\Plugins\CustomBladePlugin;
 use App\Services\SessionManagerService;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Prompts\Output\ConsoleOutput;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        // $this->app->singleton('session-manager', function ($app) {
-        //     return new SessionManagerService();
-        // });
-        $this->app->alias('session-manager', SessionManagerService::class);
-    
-    }
+	/**
+	 * Register any application services.
+	 */
+	public function register(): void
+	{
+		// $this->app->singleton('session-manager', function ($app) {
+		//     return new SessionManagerService();
+		// });
+		$this->app->alias('session-manager', SessionManagerService::class);
+	}
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        CustomBladePlugin::register();
-    }
+	/**
+	 * Bootstrap any application services.
+	 */
+	public function boot(): void
+	{
+		$this->app->bind('console-output', function ($app) {
+			return new ConsoleOutput();
+		});
+	}
 }
