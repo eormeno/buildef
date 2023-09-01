c2701a313d9401ebc6b451d941f00e1000f866e2
DAMO\Emilio <eormeno@gmail.com>
2023-09-01 10:37:12 -0300

    changes

--- app/buildef/Buildef.php
+++ app/buildef/Buildef.php
@@ -7,55 +7,62 @@ use Illuminate\Support\Facades\File;
 class Buildef
 {
 
-	public static function start()
-	{
-		$consoleOutput = app('console-output');
-		$path = self::getHtmlTemplatesDir();
+    public static function start()
+    {
+        $consoleOutput = app('console-output');
+        $path = self::getBuildefTemplatesDir();
 
-		$templatesFiles = self::getFiles($path, 'html');
+        $templatesFiles = self::getFiles($path, 'xml');
 
-		foreach ($templatesFiles as $templateFile) {
-			$template = self::parseXmlFile($templateFile->getPathname());
-			$consoleOutput->writeln('Template: ' . $template);
-		}
-	}
+        foreach ($templatesFiles as $templateFile) {
+            try {
+                $template = self::parseXmlFile($templateFile->getPathname());
+                $consoleOutput->writeln($template['name'] . ' is ' . $template['is']);
+            } catch (\Exception $e) {
+                $consoleOutput->writeln($e->getMessage());
+            }
+        }
+    }
 
-	private static function parseXmlFile($file)
-	{
-		$xml = simplexml_load_file($file);
-		if (!$xml) {
-			throw new \Exception('Error parsing XML template file: ' . $file);
-		}
-		$template_name = $xml['name'];
-		$template_params = self::parseTemplateParams($xml);
-		return $template_name;
-	}
+    private static function parseXmlFile($file)
+    {
+        $xml = simplexml_load_file($file);
+        $rootName = $xml->getName();
+        $attributeIs = $xml->attributes()['is'] ?? '(root)';
 
-	private static function parseTemplateParams($xml)
-	{
-		$params = [];
-		foreach ($xml->param as $param) {
-			$params[] = [
-				'name' => $param['name'],
-				'type' => $param['type'],
-				'default' => $param['default'],
-				'readonly' => $param['readonly'] === 'true',
-			];
-		}
-		return $params;
-	}
+        $template = [
+            'name' => $rootName,
+            'is' => $attributeIs,
+        ];
+        return $template;
+    }
 
-	private static function getFiles($path, $extension)
-	{
-		$files = File::allFiles($path);
-		$filteredFiles = collect($files)->filter(function ($file) use ($extension) {
-			return $file->getExtension() === $extension;
-		});
-		return $filteredFiles;
-	}
+    private static function parseTemplateParams($xml)
+    {
+        $params = [];
+        foreach ($xml->param as $param) {
+            $params[] = [
+                'name' => $param['name'],
+                'type' => $param['type'],
+                'default' => $param['default'],
+                'readonly' => $param['readonly'] === 'true',
+            ];
+        }
+        return $params;
+    }
 
-	private static function getHtmlTemplatesDir()
-	{
-		return dirname(__FILE__) . '/renders/html';
-	}
+    private static function getFiles($path, $extension)
+    {
+        $files = File::allFiles($path);
+        $filteredFiles = collect($files)->filter(function ($file) use ($extension) {
+            return $file->getExtension() === $extension;
+        });
+        return $filteredFiles;
+    }
+
+    private static function getBuildefTemplatesDir()
+    {
+        $viewLogicPath = base_path() . '/view-logic';
+        return $viewLogicPath;
+    }
 }
